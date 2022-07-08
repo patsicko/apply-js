7b6934e6be4fe70a2d85b9b9d31f085644ef8c26
patsicko <patsicko@g>
2022-07-08 22:00:14 +0200

    Feat:Apply

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let lname=input[1];
 let email=input[2];
 let password=input[3];
 let education=document.getElementById("education");
-// let value=education.options[education.selectedIndex].value;
+
 let photo=document.getElementById("photo");
 let submit=document.getElementById("submit");
 let congs=document.querySelector(".congs");
@@ -32,7 +32,9 @@ submit.addEventListener("click",e=>{
     congs.classList.toggle("visible");
 
     if(fname.value && lname.value && email.value && password.value){
-        congs.textContent=` Congratulations ${fname.value} ${lname.value}!!! your application is successfully received `;
+        congs.textContent=` Congratulations ${fname.value} ${lname.value}!!!
+         your application is successfully received. You will obtain a feedback within 48 hours`;
+        console.log(`${fname.id}:${fname.value} ${lname.id}:${lname.value} ${email.id}:${email.value} ${password.id}:${password.value} ${education.id}:${education.value}`)
     }
   else{
     congs.textContent=`please, fill all the fields correctly!!`
